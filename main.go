a55c50ff01fd9424de8de745a11c2971b1c48bc7
Oğuzhan Durgut <oguzhan.durgut@btsgrp.com>
2025-02-20 17:03:53 +0300

    feat(api): implement rest api with gin-gonic

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type Response struct {
+	Msg string `json:"message"`
+}
+
 func main() {
-
-	http.HandleFunc("GET /ping", handlePing)
-
-	log.Println("Server listening...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
-
+	router := gin.Default()
+	router.GET("/ping", handlePing)
+	router.Run(":8000")
 }
 
-func handlePing(w http.ResponseWriter, r *http.Request) {
-	log.Println("Request recieved")
+func handlePing(c *gin.Context) {
+	res := Response{Msg: "pong"}
+	c.JSON(http.StatusOK, res)
 }
+
+// Vanilla implementation
+// func main() {
+// 	http.HandleFunc("GET /ping", handlePing)
+// 	log.Println("Server listening...")
+// 	log.Fatal(http.ListenAndServe(":8000", nil))
+// }
+
+// func handlePing(w http.ResponseWriter, r *http.Request) {
+// 	res := Response{Msg: "pong"}
+// 	json.NewEncoder(w).Encode(res)
+// 	w.WriteHeader(http.StatusOK)
+// 	log.Println("Request recieved")
+// }
